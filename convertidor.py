f3b112a9c9d693ff52a10d6a289f5e681b5ceef3
Mumanator <jeramix92@gmail.com>
2023-01-27 09:32:46 -0500

    Implementing test and documentation

--- convertidor.py
+++ convertidor.py
@@ -2,10 +2,20 @@ import string
 
 alpha = {}
 def convertidor(palabra):
+    """This function transforms text strings into binary code.
+
+    Args:
+        palabra (str): binary code
+
+    >>> convertidor('Anna')
+    0b1, 0b1110, 0b1110, 0b1, 
+    """
     palabra = palabra.lower()
     for i, letra1 in enumerate(string.ascii_lowercase):
         alpha[letra1] = i+1
     for letra in palabra:
         print(bin(alpha.get(letra)), end=', ')
 
-convertidor('Anna')
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
